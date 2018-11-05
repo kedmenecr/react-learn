edd47167f4eaaff61ba2d4a7fa0162eae864276a
kedmenecr <kedmenec.roberto@gmail.com>
2018-11-05 08:51:31 +0100

    Toying around again.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
+
+const Body = () =>(
+	<div>
+		Some text from function jsx 
+	</div>
+)
+
+class Header extends Component {
+	render(){
+		return(
+			<header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi <code>src/App.js</code> and save to reload.
           </p>
+					<Body/>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -20,6 +27,15 @@ class App extends Component {
             Learn React
           </a>
         </header>
+		)
+	}
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+				<Header/>
       </div>
     );
   }
