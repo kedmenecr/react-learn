6bacf23a83a92b17df6165731aedd9ec109b8050
kedmenecr <kedmenec.roberto@gmail.com>
2018-11-05 09:07:30 +0100

    pushing data to components.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-
-const Body = () =>(
-	<div>
-		Some text from function jsx 
-	</div>
+const Body = () => (
+  <div>
+    Some text from function jsx{' '}
+  </div>
 )
 
 class Header extends Component {
-	render(){
-		return(
-			<header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi <code>src/App.js</code> and save to reload.
-          </p>
-					<Body/>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-		)
-	}
-}
-
-class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-				<Header/>
-      </div>
-    );
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          {this.props.title}
+        </p>
+        <p>
+          {this.props.num}
+        </p>
+        <p>
+          {this.props.myArr[0]}
+        </p>
+        <p>
+          {JSON.stringify(this.props.myObj)}
+        </p>
+				<p>
+					{this.props.myFunc(100,10)}
+				</p>
+        <Body />
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
+    )
   }
 }
 
-export default App;
+class App extends Component {
+  render () {
+    const add = (a, b) => a + b
+    return (
+      <div className='App'>
+        <Header
+          title='Hello from App'
+          num={5}
+          myObj={{
+            a: 5,
+            b: 6
+          }}
+					myArr={[1, 2, 3, 4, 5]}
+					myFunc={add}
+        />
+      </div>
+    )
+  }
+}
+
+export default App
